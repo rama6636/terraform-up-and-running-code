6820469b1a1bc4b9be7bf0b8df8c21c193a8a839
Mark Kerzner <mark@elephantscale.com>
2020-06-15 17:44:05 -0500

    TERRAFORM: lab 2

--- code/terraform/01-why-terraform/web-server/step2/main.tf
+++ code/terraform/01-why-terraform/web-server/step2/main.tf
@@ -8,7 +8,7 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "instance1" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
@@ -34,6 +34,6 @@ resource "aws_security_group" "instance" {
 }
 
 output "public_ip" {
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.instance1.public_ip
   description = "The public IP address of the web server"
 }
